86e39e75699f0a421634aa24f5a3714c8e4d94b3
benrconway84 <benrconway84@gmail.com>
2018-12-04 08:23:30 +0000

    corrected latlngs for all locations

--- resources/data.js
+++ resources/data.js
@@ -9,7 +9,7 @@ const categories = ["Cafe", "Restaurants", "Markets", "Shopping", "Classes"]
 const baseData = [
   {
     name: "Fortitude Coffee",
-    location: {lat: 55.9557693, lng: -3.1946877},
+    location: {lat: 55.95578, lng: -3.1930675},
     address: "3C York Pl, Edinburgh EH1 3EB",
     rating: 3.5,
     website: "http://www.fortitudecoffee.com/",
@@ -17,18 +17,18 @@ const baseData = [
           " not buggy accessible",
     type: "cafe"
   },
-  { //Chocolate Tree
+  {
     name: "Chocolate Tree",
-    location: {lat: 55.9367107, lng: -3.2095956},
+    location: {lat: 55.936714, lng: -3.2079645},
     address: "123 Bruntsfield Pl, Edinburgh EH10 4EQ",
     rating: 3.5,
     website: "https://www.choctree.co.uk/",
     info: "Best hot chocolate in town, cosy, so book in advance!",
     type: "cafe"
   },
-  { // machina espresso
+  {
     name: "Machina Espresso",
-    location: {lat:55.9426817, lng:-3.203282},
+    location: {lat: 55.942674, lng: -3.2016435},
     address: "2 Brougham Pl, Tollcross, Edinburgh EH3 9HW",
     rating: 4,
     website: "machina-coffee.co.uk",
@@ -36,27 +36,27 @@ const baseData = [
           "to make it all with.",
     type: "cafe"
   },
-  { // The Walnut
+  {
     name: "The Walnut",
-    location: {lat: 55.9617824, lng: -3.1827168},
+    location: {lat: 55.961766, lng: -3.1810195},
     address: "9 Croall Pl, Edinburgh EH7 4LT",
     rating: 4,
     website: "https://www.facebook.com/TheWalnutEdinburgh/",
     info: "Great food, great space, staff are hit and miss though",
     type: "restaurants"
   },
-  { // Three Birds
+  {
     name: "Three Birds",
-    location: {lat: 55.9368189, lng: -3.2102495},
+    location: {lat: 55.936817, lng: -3.2086095},
     address: "3-5 Viewforth, Edinburgh EH10 4JD",
     rating: 5,
     website: "http://www.threebirds.co.uk/",
     info: "Great food, great space, great people. Booking is essential",
     type: "restaurants"
   },
-  { // TimberYard
+  {
     name: "TimberYard",
-    location: {lat: 55.946412, lng:-3.203936},
+    location: {lat: 55.94641, lng: -3.2022955},
     address: "10 Lady Lawson St, Edinburgh EH3 9DS",
     rating: 3.5,
     website: "timberyard.co",
@@ -64,9 +64,9 @@ const baseData = [
           " industrial-chic former warehouse space.",
     type: "restaurants"
   },
-  { // Gardener's Cottage
+  {
     name: "Gardener's Cottage",
-    location: {lat: 55.9574752, lng: -3.1828547},
+    location: {lat: 55.95751, lng: -3.1812155},
     address: "1 Royal Terrace Gardens, London Rd, Edinburgh EH7 5DX",
     rating: 3.5,
     website: "https://www.thegardenerscottage.co/",
@@ -74,63 +74,63 @@ const baseData = [
           " made using sustainable local produce.",
     type: "restaurants"
   },
-  { // Wizard Drops
+  {
     name: "Wizard Drops",
-    location: {lat: 55.9608771, lng: -3.182836},
+    location: {lat: 55.960876, lng: -3.1812065},
     address: "105 Brunswick St, Edinburgh EH7 5HR",
     rating: 4,
     website: "https://www.facebook.com/Wizardrops-752335164807445/",
     info: "Aromatherapy, soaps, shampoos, cleaners, all natural, all here.",
     type: "shopping"
   },
-  { // Stockbridge Markets
+  {
     name: "Stockbridge Markets",
-    location: {lat: 55.957527, lng: -3.2105223},
+    location: {lat: 55.957577, lng: -3.2089115},
     address: "Saunders St, Edinburgh EH3 6TQ",
     rating: 4.5,
     website: "http://www.stockbridgemarket.com/",
     info: "Great markets, open Sundays 10am-5pm.",
     type: "markets"
   },
-  { // Castle Terrace Markets
+  {
     name: "Castle Terrace Markets",
-    location: {lat: 55.947786,lng: -3.205703},
+    location: {lat: 55.947779,lng: -3.2040625},
     address: "Castle Terrace, Edinburgh EH1 2EN",
     rating: 4,
     website: "http://www.edinburghfarmersmarket.co.uk/",
     info: "Great variety of produce, open Saturday 9am-2pm.",
     type: "markets"
   },
-  { // Cat's Miaou
+  {
     name: "Cat's Miaou",
-    location: {lat: 55.959689, lng: -3.184711},
+    location: {lat: 55.959687, lng: -3.18305351},
     address: "36 Elm Row, Edinburgh EH7 4AH",
     rating: 3.5,
     website: "http://www.thecatsmiaou.co.uk/",
     info: "Quirky and unusual, cards, gifts and more",
     type: "shopping"
   },
-  { // Kuk Sool Won 55.970814,-3.1762435
+  {
     name: "Kuk Sool Won",
-    location: {lat: 55.970848, lng: -3.1779271},
+    location: {lat: 55.970814, lng: -3.1762435},
     address: "82 Jane St, Edinburgh EH6 5HG",
     rating: 4,
     website: "http://www.martialarts-ksw.co.uk/",
     info: "Learn this Korean Martial Art here in Edinburgh!",
     type: "classes"
   },
-  { // rick young 55.970577,-3.173358
+  {
     name: "Rick Young's Black Belt Academy",
-    location: {lat: 55.9705895, lng: -3.1733582},
+    location: {lat: 55.970577, lng: -3.173358},
     address: "9A Duke St, Edinburgh EH6 8HG",
     rating: 4,
     website: "http://www.rick-young.co.uk/",
     info: "Professional level classes in a variety of Martial arts.",
     type: "classes"
   },
-  { // santosa yoga 55.962491,-3.1774705
+  {
     name: "Santosa Yoga",
-    location: {lat: 55.962491, lng: -3.1774705},
+    location: {lat: 55.962491, lng: -3.17747055},
     address: "21 Albert St, Edinburgh EH7 5LH",
     rating: 4,
     website: "https://studio.santosa.co.uk/",
@@ -138,9 +138,9 @@ const baseData = [
           "and a cafe full of delicious sweeties!",
     type: "classes"
   },
-  { // Gathering Essence 55.957347,-3.1882825
+  {
     name: "Gathering Essence",
-    location: {lat: 55.9574466, lng: -3.1899461},
+    location: {lat: 55.957347, lng: -3.1882825},
     address: "14A Broughton St Ln, Edinburgh EH1 3LY",
     rating: 4,
     website: "http://www.gatheringessence.com/",
