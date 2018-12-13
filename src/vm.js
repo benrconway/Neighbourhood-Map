604c8af74c4765bc87d3843b3d6f55af45705649
benrconway84 <benrconway84@gmail.com>
2018-12-13 07:57:47 +0000

    returned markers to the map

--- src/vm.js
+++ src/vm.js
@@ -37,33 +37,28 @@ function initMap() {
   });
 }
 
-function init() {
-  initMap();
-  // createElements(vm.listData());
-  // displayElements(markers);
-}
-
 function createElements(listData) {
   // before rendering, clear all the markers from the map
   clearMarkers();
   clearInfoWindows();
+  // setup bounds so that the map can change on each render
   let bounds = new google.maps.LatLngBounds();
 
-  listData.forEach((item) => {
-    bounds.extend(item.location);
-    renderMarker(item);
+  listData.forEach((item, index) => {
+    bounds.extend(item.location());
+    renderMarker(item, index);
     }
   )
   map.fitBounds(bounds)
 }
 
 // Creates markers for a given array of values.
-function renderMarker(listData) {
+function renderMarker(item, index) {
   let marker = new google.maps.Marker({
-    position: item.location,
-    title: item.name,
-    id: `list-item-${index}`,
-    icon: `../resources/images/icons/${item.type}-off.png`
+    position: item.location(),
+    title: item.name(),
+    id: index,
+    icon: `../resources/images/icons/${item.type()}-off.png`
   });
   marker.setMap(map);
   // change icon to indicate which has been nominated.
@@ -76,13 +71,12 @@ function renderMarker(listData) {
   markers.push(marker);
 }
 
-function createInfoWindow(item) {
-  let index = listData.indexOf(item);
-  let content = `<a href="${item.website}" target="_blank"><h3>` +
-  `${item.name}</h3></a><p>${item.info}<br> `+
+function createInfoWindow(item, index) {
+  let content = `<a href="${item.website()}" target="_blank"><h3>` +
+  `${item.name()}</h3></a><p>${item.info()}<br> `+
   `<div><span>Current Weather:</span><canvas id="weather-info-${index}"` +
   ` width="32" height="32"></canvas></div>` +
-  `Rating:${item.rating}/5</p>`
+  `Rating:${item.rating()}/5</p>`
   return new google.maps.InfoWindow({
     content: content
   })
@@ -98,6 +92,11 @@ function clearInfoWindows() {
   infowindows = [];
 }
 
+function init() {
+  initMap();
+  createElements(vm.listData());
+  // displayElements(markers);
+}
 
-
+init()
 ko.applyBindings(vm);
